9362e774d9c97209841fbe30a1e2a40daf2d73dd
Matt Davis <jiffyclub@gmail.com>
2015-05-01 10:53:02 -0700

    initial commit of caldwr

---
+++ README.rst
@@ -0,0 +1,6 @@
+California Department of Water Resources Python Data Tools
+==========================================================
+
+Python tools for parsing data from the California
+Department of Water Resources'
+`data site <http://cdec.water.ca.gov/index.html>`__.
---
+++ setup.py
@@ -0,0 +1,29 @@
+from ez_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
+
+with open('README.rst', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='caldwr',
+    version='1.0dev',
+    description=(
+        'Tools for retrieving and parsing data from California DWR'),
+    long_description=long_description,
+    author='Matt Davis',
+    author_email='jiffyclub@gmail.com',
+    url='https://github.com/jiffyclub/caldwr',
+    packages=find_packages(),
+    tests_require=['pytest >= 2.4'],
+    classifiers=[
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4'])
